4896346c657f1f1cda2fb9acd0e27d0c1b933333
GaniCodes29 <gani29082004@gmail.com>
2024-04-18 14:53:43 +0530

    mjs

--- NodeJS/http.js
+++ NodeJS/http.js
@@ -1,17 +1,32 @@
-let  nameElement = document.querySelector('.name');
-let  SurnameElement = document.querySelector('.Surname');
+
+let nameElement = document.querySelector('.name');
+let surnameElement = document.querySelector('.surname')
+let surnameInput = document.querySelector('#surnameInput');
+
 const getMyName = async () => {
-    let res = await fetch('http://127.0.0.1.3000/')
+    let res = await fetch('http://127.0.0.1:3000/')
     console.log(res);
     if(!res.ok) {
-        alert('failed to get name');
+        alert('Failed to get name');
         return;
     }
     let name = await res.text();
     nameElement.textContent = name;
 }
 const postSurname = async () => {
-    let res = await fetch('http://127.0.0.1.3000/', {
-    
+    let mysurname = surnameInput.value;
+    let res = await fetch('http://127.0.0.1:3000/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({surname: Srinivas})
+    })
+    // console.log(res);
+    if(!res.ok) {
+        alert('Failed to post surname');
+        return;
     }
+    let { surname } = await res.json();
+    surnameElement.textContent = surname;
 }
